8196869b3c4f9ac18c0a87321a161535fec12418
Fernanda2209 <mafe02095@gmail.com>
2023-01-18 00:19:25 -0500

    Images and home page

--- react-portfolio-master/src/scenes/MySkills.jsx
+++ react-portfolio-master/src/scenes/MySkills.jsx
@@ -35,11 +35,7 @@ const MySkills = () => {
               className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
               before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
             >
-              <img
-                alt="skills"
-                className="z-10"
-                src="assets/skills-image.png"
-              />
+             
             </div>
           ) : (
             <img alt="skills" className="z-10" src="assets/skills-image.png" />
