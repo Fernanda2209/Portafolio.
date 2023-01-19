00a68614068b5a80e91da6864cb05ebba5a30827
Fernanda2209 <mafe02095@gmail.com>
2023-01-19 01:00:40 -0500

    Project 1 and Project 2

--- react-portfolio-master/src/scenes/Projects.jsx
+++ react-portfolio-master/src/scenes/Projects.jsx
@@ -1,5 +1,7 @@
 import LineGradient from "../components/LineGradient";
 import React from 'react';
+import Amigurumis from '../assets/Amigurumis.jpeg';
+import Mortal from '../assets/Mortal.png';
 import { motion } from "framer-motion";
 const container = {
   hidden: {},
@@ -50,7 +52,7 @@ const Projects = () => {
         >
           {/* ROW 1 */}
           <div
-            className="flex justify-center text-center items-center p-10 bg-red
+            className="flex justify-center text-center items-center p-10 
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
             THE EVOLUTION OF MY WORK
@@ -60,9 +62,36 @@ const Projects = () => {
             <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
            bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
               <p className="text-2xl font-playfair">Amigurumis Land</p>
+              <p className="mt-7"> Amigurumis land is a project created with react, it was focused on
+               a real customer and on the promotion of economic growth within my environment. </p>
+            </div>
+           <img className="flex justify-center text-center items-center p-10 
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+               src={Amigurumis} />
+          </div>
+
+
+          <div className="relative opacity: 1; transform: none" >
+            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
+              <p className="text-2xl font-playfair">Mortal Dev</p>
+              <p className="mt-7"> Moratl dev is a game based on the sacrifice of developers, it was a collaborative
+               project in which the main language was javascript. </p>
+            </div>
+           <img className="flex justify-center text-center items-center p-10 
+              max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
+               src={Mortal} />
+          </div>
+
+          {/* ROW 2 */}
+
+          <div className="relative opacity: 1; transform: none" >
+            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
+              <p className="text-2xl font-playfair">Amigurumis Land</p>
               <p className="mt-7"> Amigurumis Land is a react project</p>
             </div>
-           <img src='../assets/project-1.jpeg' alt="MALDITA IMAGEN" />
+           <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
           </div>
 
 
@@ -74,50 +103,8 @@ const Projects = () => {
             </div>
            <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
           </div>
-
-
-          <div className="relative opacity: 1; transform: none" >
-            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
-              <p className="text-2xl font-playfair">Amigurumis Land</p>
-              <p className="mt-7"> Amigurumis Land is a react project</p>
-            </div>
-           <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
-          </div>
-
-
-          <div className="relative opacity: 1; transform: none" >
-            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
-              <p className="text-2xl font-playfair">Amigurumis Land</p>
-              <p className="mt-7"> Amigurumis Land is a react project</p>
-            </div>
-           <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
-          </div>
-          <div className="relative opacity: 1; transform: none" >
-            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
-              <p className="text-2xl font-playfair">Amigurumis Land</p>
-              <p className="mt-7"> Amigurumis Land is a react project</p>
-            </div>
-           <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
-          </div>
-          <div className="relative opacity: 1; transform: none" >
-            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
-              <p className="text-2xl font-playfair">Amigurumis Land</p>
-              <p className="mt-7"> Amigurumis Land is a react project</p>
-            </div>
-           <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
-          </div>
-          <div className="relative opacity: 1; transform: none" >
-            <div className="absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-           bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue">
-              <p className="text-2xl font-playfair">Amigurumis Land</p>
-              <p className="mt-7"> Amigurumis Land is a react project</p>
-            </div>
-           <img src="../assets/project-1.jpeg" alt="MALDITA IMAGEN" />
-          </div>
+          
+          
           
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
